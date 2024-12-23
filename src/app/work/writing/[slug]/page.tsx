c29b36769f925ead5defd669e989c4d63da605a3
some-molasses <river.stanley173@gmail.com>
2024-12-23 13:31:13 -0500

    Writing formatting with markdown

--- src/app/work/writing/[slug]/page.tsx
+++ src/app/work/writing/[slug]/page.tsx
@@ -1,6 +1,11 @@
 import { Content } from "@/app/components/content/content";
+import { Footer } from "@/app/components/footer/footer";
 import { Page } from "@/app/components/page/page";
 import { Titles } from "@/app/components/titles/titles";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
+import { WritingIndexer } from "../../../../../writing-database/indexer";
+import "./writing.scss";
 
 export default async function WritingWork({
   params,
@@ -9,10 +14,27 @@ export default async function WritingWork({
 }) {
   const { slug } = await params;
 
+  const work = await WritingIndexer.getWriting(
+    `mathnews/v156/i6/the-last-rabbit`,
+  );
+
+  if (work === null) {
+    return null;
+  }
+
+  const { title, subtitle, date, body } = work;
+
+  const result = (await remark().use(remarkHtml).process(body)).toString();
+
   return (
     <Page id="writing-work-page">
       <Content>
-        <Titles title={`titulo ${slug}`} subtitle="subtitulo" />
+        <Titles title={title} subtitle={subtitle} />
+        <div
+          id="writing-work-body"
+          dangerouslySetInnerHTML={{ __html: result }}
+        ></div>
+        <Footer />
       </Content>
     </Page>
   );
---
+++ writing-database/mathnews/v156/i6/sinusoidal-soup/main.md
@@ -0,0 +1,24 @@
+**Ingredients**
+
+- some \\(n\\) tomatoes
+- 1 chopped onion
+- garlic, chopped
+- spices?
+- several hot peppers, chopped (e.g. jalapeños, chili peppers, whatever you can find really)
+- an entire handful of cold herbs, chopped (e.g. basil, parsley, cilantro, mint)
+- whatever else u want \<3
+
+**Instructions**
+
+1. in a large pot, add chopped onion, garlic, and spices. Stir and let cook for the next few minutes.
+2. add tomatoes, hot peppers, and leaves
+3. let cook for approximately 20 minutes, or whenever the tomatoes have sufficiently broken down
+4. serve with kleenex
+
+it’s called sinusoidal soup because it makes your sinuses go “oi” immediately before experiencing the entire range of heat to cold at once. It’s a bit like
+
+\\\[  
+\\text{very hot} \+ \\text{very cold} \= \\text{very hot} \+ \\text{very $\\neg$hot} \= \\text{very}  
+\\\]
+
+strongly recommended if you ever feel like clearing out your entire head.
