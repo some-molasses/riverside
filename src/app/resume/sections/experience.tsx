f6a6b9b8e50fae0327179f82b3c6f74c34c64a47
some-molasses <river.stanley173@gmail.com>
2024-12-31 13:45:50 -0500

    Add resume style

--- src/app/resume/sections/experience.tsx
+++ src/app/resume/sections/experience.tsx
@@ -8,174 +8,181 @@ export const ExperienceSection: React.FC = () => {
   return (
     <section id="experience-section">
       <Heading2>Experience</Heading2>
-      <ExperienceItem
-        name="Software Developer"
-        team="Cards"
-        date="Summer 2024"
-        company="Wealthsimple"
-      >
-        <li>
-          Played a key role in implementing a card rewards engine overhaul,{" "}
-          <HL terms="backend">processing 80 000+ daily transactions</HL> and
-          enabling features including{" "}
-          <HL terms="backend">ATM reimbursements</HL>, cashback, and
-          account-level rewards
-        </li>
-        <li>
-          <HL terms="project">Independently architected and implemented</HL> a
-          rewards summarization system, delivering real-time, dynamic product
-          content in-app to overcome mobile app update challenges
-        </li>
-        <li>
-          <HL terms="backend project">
-            Oversaw the migration of 1000+ credit cards
-          </HL>{" "}
-          to a new engine while ensuring minimal user disruption
-        </li>
-        <li>
-          Proactively addressed and resolved{" "}
-          <HL terms="quality">cross-team concerns and system errors</HL>,
-          reducing service failure resolution times
-        </li>
-      </ExperienceItem>
-      <ExperienceItem
-        name="Backend Engineer"
-        team="Search"
-        date="Winter 2024"
-        company="Faire"
-      >
-        <li>
-          <HL terms="backend">Implemented processes</HL> using{" "}
-          <HL terms="database">DynamoDB</HL> and <HL terms="database">Redis</HL>{" "}
-          to process up to{" "}
-          <HL terms="database">1.8 M real-time search update records daily</HL>
-        </li>
-        <li>
-          <HL terms="project">Led a full-stack initiative</HL> to split
-          non-essential logic out of the search critical path, leading to a{" "}
-          <HL terms="project">~8% improvement towards our target latency</HL>
-        </li>
-        <li>
-          <HL terms="project backend">
-            Investigated and implemented parallelization improvements
-          </HL>{" "}
-          across a complex, mature search infrastructure to achieve sufficient
-          performance gains to offset high-latency changes elsewhere
-        </li>
-        <li>
-          Implemented and monitored <HL terms="quality">metrics dashboards</HL>{" "}
-          through <HL terms="quality">Datadog</HL> to ensure services operated
-          at target levels
-        </li>
-      </ExperienceItem>
-      <ExperienceItem
-        name="Frontend Engineer"
-        team="Emails"
-        date="Winter 2023"
-        company="Faire"
-      >
-        <li>
-          <HL terms="project">Managed the implementation</HL> of experimental
-          features, coordinating engineers and designers to achieve goals
-        </li>
-        <li>
-          <HL terms="project">Independently scoped and implemented</HL>{" "}
-          <HL terms="a11y">WCAG 2.1 accessibility features</HL>, drawing praise
-          from external teams
-        </li>
-        <li>
-          <HL terms="project">Outlined and implemented</HL> incident{" "}
-          <HL terms="quality">post-mortem recommendations</HL>, improving{" "}
-          engineering <HL terms="quality">quality</HL> practices to{" "}
-          <HL terms="quality">prevent production failures</HL>
-        </li>
-        <li>
-          Contributed to overhauling a mature{" "}
-          <HL terms="react frontend">React</HL>-based web app to use modern
-          React state management patterns with{" "}
-          <HL terms="frontend quality">Jest</HL> unit testing and{" "}
-          <HL terms="frontend quality">Cypress</HL> end-to-end testing.
-        </li>
-      </ExperienceItem>
-      <ExperienceItem
-        name="Software Developer"
-        team="Web"
-        date="Winter 2022"
-        company="Vidyard"
-      >
-        <li>
-          Led integration of <HL terms="frontend quality">Jest</HL>,{" "}
-          <HL terms="frontend quality">ESLint</HL>, and{" "}
-          <HL terms="frontend typescript">TypeScript</HL> with a mature
-          codebase, starting the team&apos;s use of{" "}
-          <HL terms="frontend quality">test-driven development</HL> with{" "}
-          <HL terms="frontend quality">28% code coverage</HL> in two months to
-          reduce and prevent user-facing errors.
-        </li>
-        <li>
-          Performed and improved on a{" "}
-          <HL terms="a11y">WCAG2.1 accessibility audit</HL> to decrease UX
-          friction and <HL terms="a11y">increase conversion</HL>.
-        </li>
-        <li>
-          Overhauled a <HL terms="php">PHP</HL> component library to implement{" "}
-          <HL terms="php">object-oriented programming</HL> principles,
-          decreasing ticket cycle time.
-        </li>
-        <li>
-          <HL terms="project">Collaborated with external teams</HL> to clarify
-          requirements and deliver progress updates.
-        </li>
-      </ExperienceItem>
-      <ExperienceItem
-        name="Software Developer"
-        date="Summer 2021"
-        company="Uptake Canada"
-      >
-        <li>
-          Independently developed <HL terms="backend">NestJS RESTful API</HL>{" "}
-          endpoints and complex <HL terms="database">SQL</HL> stored procedures
-          to support frontend specifications
-        </li>
-        <li>
-          Developed <HL terms="frontend javascript">new frontend interfaces</HL>{" "}
-          using ExtJS to support design specifications and display API data.
-        </li>
-      </ExperienceItem>
-      <ExperienceItem
-        name="Web Developer"
-        date="Summer 2020"
-        company="Salus Global"
-      >
-        <li>
-          <HL terms="frontend design">
-            Designed and developed a mobile-friendly online eLearning platform
-          </HL>{" "}
-          for an international client with 2300+ total users, using{" "}
-          <HL terms="frontend html">HTML</HL>, <HL terms="frontend css">CSS</HL>
-          , <HL terms="frontend javascript">JavaScript</HL>, and an internal
-          language.
-        </li>
-        <li>
-          Entrusted with implementing <HL terms="frontend css">CSS</HL> with
-          minimal HTML changes to render an existing platform with 2500+ users{" "}
-          <HL terms="frontend css">mobile-aware</HL>
-        </li>
-        <li>
-          Developed and implemented{" "}
-          <HL terms="frontend javascript">JavaScript web components</HL> to{" "}
-          <HL terms="frontend javascript">promote user engagement</HL> with
-          educational materials
-        </li>
-        <li>
-          <HL terms="project design">Led the visual design work</HL> for a
-          mobile eLearning app deployed to 10 000+ users
-        </li>
-        <li>
-          Edited supervisors&apos; external communications to ensure greater
-          professionalism and impact in dealings with clients.
-        </li>
-      </ExperienceItem>
+      <div>
+        <ExperienceItem
+          name="Software Developer"
+          team="Cards"
+          date="Summer 2024"
+          company="Wealthsimple"
+        >
+          <li>
+            Played a key role in implementing a card rewards engine overhaul,{" "}
+            <HL terms="backend">processing 80 000+ daily transactions</HL> and
+            enabling features including{" "}
+            <HL terms="backend">ATM reimbursements</HL>, cashback, and
+            account-level rewards
+          </li>
+          <li>
+            <HL terms="project">Independently architected and implemented</HL> a
+            rewards summarization system, delivering real-time, dynamic product
+            content in-app to overcome mobile app update challenges
+          </li>
+          <li>
+            <HL terms="backend project">
+              Oversaw the migration of 1000+ credit cards
+            </HL>{" "}
+            to a new engine while ensuring minimal user disruption
+          </li>
+          <li>
+            Proactively addressed and resolved{" "}
+            <HL terms="quality">cross-team concerns and system errors</HL>,
+            reducing service failure resolution times
+          </li>
+        </ExperienceItem>
+        <ExperienceItem
+          name="Backend Engineer"
+          team="Search"
+          date="Winter 2024"
+          company="Faire"
+        >
+          <li>
+            <HL terms="backend">Implemented processes</HL> using{" "}
+            <HL terms="database">DynamoDB</HL> and{" "}
+            <HL terms="database">Redis</HL> to process up to{" "}
+            <HL terms="database">
+              1.8 M real-time search update records daily
+            </HL>
+          </li>
+          <li>
+            <HL terms="project">Led a full-stack initiative</HL> to split
+            non-essential logic out of the search critical path, leading to a{" "}
+            <HL terms="project">~8% improvement towards our target latency</HL>
+          </li>
+          <li>
+            <HL terms="project backend">
+              Investigated and implemented parallelization improvements
+            </HL>{" "}
+            across a complex, mature search infrastructure to achieve sufficient
+            performance gains to offset high-latency changes elsewhere
+          </li>
+          <li>
+            Implemented and monitored{" "}
+            <HL terms="quality">metrics dashboards</HL> through{" "}
+            <HL terms="quality">Datadog</HL> to ensure services operated at
+            target levels
+          </li>
+        </ExperienceItem>
+        <ExperienceItem
+          name="Frontend Engineer"
+          team="Emails"
+          date="Winter 2023"
+          company="Faire"
+        >
+          <li>
+            <HL terms="project">Managed the implementation</HL> of experimental
+            features, coordinating engineers and designers to achieve goals
+          </li>
+          <li>
+            <HL terms="project">Independently scoped and implemented</HL>{" "}
+            <HL terms="a11y">WCAG 2.1 accessibility features</HL>, drawing
+            praise from external teams
+          </li>
+          <li>
+            <HL terms="project">Outlined and implemented</HL> incident{" "}
+            <HL terms="quality">post-mortem recommendations</HL>, improving{" "}
+            engineering <HL terms="quality">quality</HL> practices to{" "}
+            <HL terms="quality">prevent production failures</HL>
+          </li>
+          <li>
+            Contributed to overhauling a mature{" "}
+            <HL terms="react frontend">React</HL>-based web app to use modern
+            React state management patterns with{" "}
+            <HL terms="frontend quality">Jest</HL> unit testing and{" "}
+            <HL terms="frontend quality">Cypress</HL> end-to-end testing.
+          </li>
+        </ExperienceItem>
+        <ExperienceItem
+          name="Software Developer"
+          team="Web"
+          date="Winter 2022"
+          company="Vidyard"
+        >
+          <li>
+            Led integration of <HL terms="frontend quality">Jest</HL>,{" "}
+            <HL terms="frontend quality">ESLint</HL>, and{" "}
+            <HL terms="frontend typescript">TypeScript</HL> with a mature
+            codebase, starting the team&apos;s use of{" "}
+            <HL terms="frontend quality">test-driven development</HL> with{" "}
+            <HL terms="frontend quality">28% code coverage</HL> in two months to
+            reduce and prevent user-facing errors.
+          </li>
+          <li>
+            Performed and improved on a{" "}
+            <HL terms="a11y">WCAG2.1 accessibility audit</HL> to decrease UX
+            friction and <HL terms="a11y">increase conversion</HL>.
+          </li>
+          <li>
+            Overhauled a <HL terms="php">PHP</HL> component library to implement{" "}
+            <HL terms="php">object-oriented programming</HL> principles,
+            decreasing ticket cycle time.
+          </li>
+          <li>
+            <HL terms="project">Collaborated with external teams</HL> to clarify
+            requirements and deliver progress updates.
+          </li>
+        </ExperienceItem>
+        <ExperienceItem
+          name="Software Developer"
+          date="Summer 2021"
+          company="Uptake Canada"
+        >
+          <li>
+            Independently developed <HL terms="backend">NestJS RESTful API</HL>{" "}
+            endpoints and complex <HL terms="database">SQL</HL> stored
+            procedures to support frontend specifications
+          </li>
+          <li>
+            Developed{" "}
+            <HL terms="frontend javascript">new frontend interfaces</HL> using
+            ExtJS to support design specifications and display API data.
+          </li>
+        </ExperienceItem>
+        <ExperienceItem
+          name="Web Developer"
+          date="Summer 2020"
+          company="Salus Global"
+        >
+          <li>
+            <HL terms="frontend design">
+              Designed and developed a mobile-friendly online eLearning platform
+            </HL>{" "}
+            for an international client with 2300+ total users, using{" "}
+            <HL terms="frontend html">HTML</HL>,{" "}
+            <HL terms="frontend css">CSS</HL>,{" "}
+            <HL terms="frontend javascript">JavaScript</HL>, and an internal
+            language.
+          </li>
+          <li>
+            Entrusted with implementing <HL terms="frontend css">CSS</HL> with
+            minimal HTML changes to render an existing platform with 2500+ users{" "}
+            <HL terms="frontend css">mobile-aware</HL>
+          </li>
+          <li>
+            Developed and implemented{" "}
+            <HL terms="frontend javascript">JavaScript web components</HL> to{" "}
+            <HL terms="frontend javascript">promote user engagement</HL> with
+            educational materials
+          </li>
+          <li>
+            <HL terms="project design">Led the visual design work</HL> for a
+            mobile eLearning app deployed to 10 000+ users
+          </li>
+          <li>
+            Edited supervisors&apos; external communications to ensure greater
+            professionalism and impact in dealings with clients.
+          </li>
+        </ExperienceItem>
+      </div>
     </section>
   );
 };
@@ -189,9 +196,16 @@ const ExperienceItem: React.FC<{
 }> = ({ name, date, company, children }) => {
   return (
     <div className="experience-item">
-      <ItemTitle title={name} bottomLeft={company} bottomRight={date} />
-      <div className="description">
-        <ul>{children}</ul>
+      <div className="item-left">
+        <div className="line-top"></div>
+        <div className="line-item"></div>
+        <div className="line-bottom"></div>
+      </div>
+      <div className="item-right">
+        <ItemTitle title={name} bottomLeft={company} bottomRight={date} />
+        <div className="description">
+          <ul>{children}</ul>
+        </div>
       </div>
     </div>
   );
--- src/app/resume/sections/projects.tsx
+++ src/app/resume/sections/projects.tsx
@@ -8,52 +8,55 @@ export const ProjectsSection: React.FC = () => {
   return (
     <section id="projects-section">
       <Heading2>Projects</Heading2>
-      <Project name="better-walks" date="2024">
-        <li>
-          Designed and developed a <HL terms="frontend">React Native</HL> mobile
-          app to navigate users through real-world scavenger hunts
-        </li>
-        <li>
-          Built a map node <HL terms="search backend">retrieval pipeline</HL> to
-          identify, rank, process, and retrieve locations of interest
-        </li>
-        <li>
-          Integrated OpenStreetMap datasets using the Overpass and
-          OpenRouteService APIs to source <HL terms="backend gis">GIS data</HL>
-        </li>
-      </Project>
-      <Project name="MathSoc Website" date="2024">
-        <li>
-          <HL terms="project">Led teams of software developers</HL> to redevelop
-          the website of the University of Waterloo Undergraduate Society of
-          Mathematics
-        </li>
-        <li>
-          Used simple technologies including <HL terms="frontend">pug</HL> and
-          JSON data storage to{" "}
-          <HL terms="quality">ensure long-term maintainability</HL>
-        </li>
-        <li>
-          Performed gradual deployment of new site to{" "}
-          <HL terms="project">over 1000 semi-regular users</HL>.
-        </li>
-      </Project>
-      <Project name="LATimes Search Engine" date="Fall 2024">
-        <li>
-          Developed an{" "}
-          <HL terms="search">indexer, ranker, and query-based summarizer</HL>{" "}
-          for a 131 000-document collection of LA Times news articles
-        </li>
-        <li>
-          Implemented{" "}
-          <HL terms="search">cosine similarity and BM25 retrieval</HL> methods
-          with optional integration of Porter stemming
-        </li>
-        <li>
-          Obtained a high grade in the associated University of Waterloo
-          elective course for which this was an assignment, MSE 541
-        </li>
-      </Project>
+      <div>
+        <Project name="better-walks" date="2024">
+          <li>
+            Designed and developed a <HL terms="frontend">React Native</HL>{" "}
+            mobile app to navigate users through real-world scavenger hunts
+          </li>
+          <li>
+            Built a map node <HL terms="search backend">retrieval pipeline</HL>{" "}
+            to identify, rank, process, and retrieve locations of interest
+          </li>
+          <li>
+            Integrated OpenStreetMap datasets using the Overpass and
+            OpenRouteService APIs to source{" "}
+            <HL terms="backend gis">GIS data</HL>
+          </li>
+        </Project>
+        <Project name="MathSoc Website" date="2024">
+          <li>
+            <HL terms="project">Led teams of software developers</HL> to
+            redevelop the website of the University of Waterloo Undergraduate
+            Society of Mathematics
+          </li>
+          <li>
+            Used simple technologies including <HL terms="frontend">pug</HL> and
+            JSON data storage to{" "}
+            <HL terms="quality">ensure long-term maintainability</HL>
+          </li>
+          <li>
+            Performed gradual deployment of new site to{" "}
+            <HL terms="project">over 1000 semi-regular users</HL>.
+          </li>
+        </Project>
+        <Project name="LATimes Search Engine" date="Fall 2024">
+          <li>
+            Developed an{" "}
+            <HL terms="search">indexer, ranker, and query-based summarizer</HL>{" "}
+            for a 131 000-document collection of LA Times news articles
+          </li>
+          <li>
+            Implemented{" "}
+            <HL terms="search">cosine similarity and BM25 retrieval</HL> methods
+            with optional integration of Porter stemming
+          </li>
+          <li>
+            Obtained a high grade in the associated University of Waterloo
+            elective course for which this was an assignment, MSE 541
+          </li>
+        </Project>
+      </div>
     </section>
   );
 };
@@ -65,9 +68,16 @@ const Project: React.FC<{
 }> = ({ name, date, children }) => {
   return (
     <div className="project-item">
-      <ItemTitle title={name} topRight={date} />
-      <div className="description">
-        <ul>{children}</ul>
+      <div className="item-left">
+        <div className="line-top"></div>
+        <div className="line-item"></div>
+        <div className="line-bottom"></div>
+      </div>
+      <div className="item-right">
+        <ItemTitle title={name} topRight={date} />
+        <div className="description">
+          <ul>{children}</ul>
+        </div>
       </div>
     </div>
   );
