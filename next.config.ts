9e0de4d0cb58467f8cece1932e3b4fdcd624fcde
River Stanley <river.stanley173@gmail.com>
2025-01-12 12:26:06 -0500

    Start trying to trace json again

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  outputFileTracingIncludes: {
+    "/api/search/items": ["./**/*.md", "./**/*.json"],
+  },
 };
 
 export default nextConfig;
