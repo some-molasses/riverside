5afbab333fcb9dbef6cd5df7bf2c992137a12d5c
River Stanley <river.stanley173@gmail.com>
2025-01-19 17:22:52 -0500

    Add mobile menu oops

--- src/app/components/top-nav/top-nav.tsx
+++ src/app/components/top-nav/top-nav.tsx
@@ -1,22 +1,52 @@
+"use client";
+
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { Typography } from "../typography/typography";
 import "./top-nav.scss";
 
+const toggleMobileMenu = () => {
+  document.getElementById("mobile-nav-items")?.classList.toggle("active");
+};
+
 export const TopNav: React.FC<{}> = () => {
+  const items = [
+    { href: "/resume", title: "employers" },
+    { href: "/portfolio?filter=writing", title: "writing" },
+    { href: "/portfolio?filter=coding", title: "coding" },
+    { href: "/portfolio", title: "portfolio" },
+  ];
+
   return (
-    <div id="top-nav">
-      <div id="top-nav-left">
-        <Link href="/">
-          <Typography variant="regular" id="nav-title">
-            river-stanley.me
-          </Typography>
-        </Link>
+    <div id="top-nav-container">
+      <div id="top-nav">
+        <div id="top-nav-left">
+          <Link href="/">
+            <Typography variant="regular" id="nav-title">
+              river-stanley.me
+            </Typography>
+          </Link>
+        </div>
+        <button id="mobile-nav" onClick={toggleMobileMenu}>
+          <FontAwesomeIcon icon={faChevronDown} />
+        </button>
+        <div id="desktop-nav-items">
+          {items.map(({ href, title }) => (
+            <NavItem href={href} key={title}>
+              {title}
+            </NavItem>
+          ))}
+        </div>
       </div>
-      <div id="top-nav-items">
-        <NavItem href="/resume">employers</NavItem>
-        <NavItem href="/portfolio?filter=writing">writing</NavItem>
-        <NavItem href="/portfolio?filter=coding">coding</NavItem>
-        <NavItem href="/portfolio">portfolio</NavItem>
+      <div id="mobile-nav-items">
+        <div id="mobile-nav-items-inner">
+          {items.map(({ href, title }) => (
+            <MobileNavItem href={href} key={title}>
+              {title}
+            </MobileNavItem>
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -29,6 +59,19 @@ const NavItem: React.FC<{
   return (
     <Link href={href} className="top-nav-item">
       <Typography variant="regular-light">{children}</Typography>
+    </Link>
+  );
+};
+
+const MobileNavItem: React.FC<{
+  children: React.ReactNode;
+  href: string;
+}> = ({ children, href }) => {
+  return (
+    <Link href={href} className="mobile-nav-item" onClick={toggleMobileMenu}>
+      <div className="contents">
+        <Typography variant="regular-light">{children}</Typography>
+      </div>
     </Link>
   );
 };
